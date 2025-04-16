f702078355b51043bc8943262980c949df3ee5e0
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2025-04-16 19:29:49 +0700

    fix: title and alias import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "@/styles/globals.css";
+import "@tailwindcss";
 
 const poppins = Poppins({
   preload: true,
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Beautiful Places in Bali",
+  title: "Tempat Wisata di Bali",
 };
 
 export default function RootLayout({
@@ -20,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.className} antialiased bg-neutral-50`}>{children}</body>
+    <html lang="id">
+      <body className={`${poppins.className} antialiased bg-neutral-50`}>
+        {children}
+      </body>
     </html>
   );
 }
